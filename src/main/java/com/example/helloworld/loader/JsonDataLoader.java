26412805fac16ebc726a0b992f7288febdd14ee1
SANOGOTECH <sanogotech@yahoo.fr>
2024-04-28 10:23:37 +0000

    Update JsonDataLoader.java

--- src/main/java/com/example/helloworld/loader/JsonDataLoader.java
+++ src/main/java/com/example/helloworld/loader/JsonDataLoader.java
@@ -1,22 +1,24 @@
 package com.example.helloworld.loader;
 
 import com.example.helloworld.model.User;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import java.io.IOException;
+import java.util.List;
 
 public class JsonDataLoader {
 
-    public User loadUserData(String resourcePath) {
+    public List<User> loadUserData(String resourcePath) {
         ObjectMapper objectMapper = new ObjectMapper();
         Resource resource = new ClassPathResource(resourcePath);
-        User user = null;
+        List<User> users = null;
         try {
-            user = objectMapper.readValue(resource.getInputStream(), User.class);
+            users = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<User>>(){});
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return user;
+        return users;
     }
 }
