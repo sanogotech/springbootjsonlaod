32ba6b210d1d282b9653222e88ce02ab2334d06d
SANOGOTECH <sanogotech@yahoo.fr>
2024-04-28 10:14:03 +0000

    Create User.java

---
+++ src/main/java/com/example/helloworld/model/User.java
@@ -0,0 +1,45 @@
+package com.example.helloworld.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // toString méthode pour afficher les informations de l'utilisateur
+    @Override
+    public String toString() {
+        return "User{" +
+               "id=" + id +
+               ", name='" + name + '\'' +
+               ", email='" + email + '\'' +
+               '}';
+    }
+}
