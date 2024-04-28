b7eef5a9759b3de077e439b89551e1e977404b2c
SANOGOTECH <sanogotech@yahoo.fr>
2024-04-28 10:22:54 +0000

    Update HelloWorldApplication.java

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,9 +1,10 @@
 package com.example.helloworld;
 
-import com.example.helloworld.loader.JsonDataLoader;
+import com.example.helloworld.utils.JsonDataLoader;
 import com.example.helloworld.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;
 
 @SpringBootApplication
 public class HelloWorldApplication {
@@ -12,7 +13,7 @@ public class HelloWorldApplication {
         SpringApplication.run(HelloWorldApplication.class, args);
         
         JsonDataLoader loader = new JsonDataLoader();
-        User user = loader.loadUserData("users.json");  // Assurez-vous que user.json est dans src/main/resources
-        System.out.println(user);
+        List<User> users = loader.loadUserData("users.json");  // Assurez-vous que users.json est dans src/main/resources
+        users.forEach(System.out::println);
     }
 }
